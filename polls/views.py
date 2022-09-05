67ac4eb947cb9c1f585eea9643bec09fd324403e
zunayed-arp <zunayed.arp@gmail.com>
2022-09-05 17:12:21 +0600

    fixed hardcoded url to dynamic in template

--- polls/views.py
+++ polls/views.py
@@ -1,27 +1,27 @@
-from django.shortcuts import render
-from django.http import HttpResponse,Http404
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 
 from polls.models import Question
 from django.template import loader
+
+
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list':latest_question_list
-    }
-    
-    
-    return render(request,"polls/index.html",context)
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
 
-def detail(request,question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request,"polls/detail.html",{'question':question})
+    return render(request, "polls/index.html", context)
 
-def results(request,question_id):
+
+def detail(request, question_id):
+
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+
+def results(request, question_id):
     response = f"You're looking at the results of {question_id}"
     return HttpResponse(response)
 
-def vote(request,question_id):
+
+def vote(request, question_id):
     return HttpResponse(f"You're voting on question {question_id}")
