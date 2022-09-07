af2b85ba1fa7e37a7adfced8528e2d9a80022102
zunayed-arp <zunayed.arp@gmail.com>
2022-09-07 20:44:57 +0600

    chore: added test case booklist view

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,24 @@
+from turtle import title
 from django.test import TestCase
+from django.urls import reverse
+from .models import Book
 
-# Create your tests here.
+class BookTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.book = Book.objects.create(
+            title="Java the complete Reference",
+            subtitle="Ninth edition",
+            author="schildt",
+            isbn="123456789JBORCL"
+        )
+    def test_book_content(self):
+        self.assertEqual(self.book.title,'Java the complete Reference')
+        self.assertEqual(self.book.subtitle,"Ninth edition")
+        self.assertEqual(self.book.author,"schildt")
+        self.assertEqual(self.book.isbn,"123456789JBORCL")
+def test_book_listview(self):
+    response = self.client.get(reverse("home"))
+    self.assertEqual(response.status_code,200)
+    self.assertEqual(response,"excellent subtitle")
+    self.assertEqual(response,"books/book_list.html")
